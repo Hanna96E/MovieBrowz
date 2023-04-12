7c792abd7d7ae1858ed338da62cba6c953f35c92
Hanna96E <hanhed-4@student.ltu.se>
2023-04-12 08:00:35 +0200

    changed name of first fragment to movie list fragment

--- app/src/main/java/com/ltu/m7019e/moviebrowz/MovieListFragment.kt
+++ app/src/main/java/com/ltu/m7019e/moviebrowz/MovieListFragment.kt
@@ -5,22 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
-import com.bumptech.glide.Glide
 import com.ltu.m7019e.moviebrowz.database.Movies
-import com.ltu.m7019e.moviebrowz.utils.Constants
-import com.ltu.m7019e.moviebrowz.databinding.FragmentFirstBinding
+import com.ltu.m7019e.moviebrowz.databinding.FragmentMovieListBinding
 import com.ltu.m7019e.moviebrowz.databinding.MovieListItemBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MovieListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentMovieListBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,9 +24,8 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
-        //_binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_list, container, false)
 
         val movies = Movies()
 
@@ -48,25 +42,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-/*        val movies = Movies()
-
-        val movieList = view.findViewById<LinearLayout>(R.id.movie_list_ll)
-        val movieItem = movieList.findViewById<View>(R.id.movie1)
-        val movieTitle = movieItem.findViewById<TextView>(R.id.movie_title)
-        val moviePoster = movieItem.findViewById<ImageView>(R.id.movie_poster)
-
-        movieTitle.text = movies.list[0].title
-
-        Glide
-            .with(this)
-            .load(Constants.POSTER_IMAGE_BASE_URL + Constants.POSTER_IMAGE_WIDTH + movies.list[0].posterPath)
-            .into(moviePoster);
-*/
-
-
-        //binding.buttonFirst.setOnClickListener {
-         //   findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        //}
     }
 
     override fun onDestroyView() {
